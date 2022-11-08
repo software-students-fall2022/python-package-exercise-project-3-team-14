328cd884b606005d0e9b8d58ea24804a37c3472a
WZ <midas0231@gmail.com>
2022-11-08 02:05:00 -0500

    Update test for hangman

--- tests/test_hangman.py
+++ tests/test_hangman.py
@@ -1,3 +1,4 @@
+from _pytest.monkeypatch import MonkeyPatch
 from english_words import english_words_set
 import random
 import string
@@ -6,7 +7,8 @@ from src.dailydose import hang
 
 class Tests:
 
-    #Test functions
+    def set_up(self):
+        self.monkeypatch = MonkeyPatch()
 
     def test_sanity_check(self):
         """
@@ -17,30 +19,30 @@ class Tests:
         actual = True  # the value we see in reality
         assert actual == expected, "Expected True to be equal to True!"
 
-    def test_hangman_length(monkeypatch):
+    def test_hangman_length(self):
         """
         Test the length of the word returned
         The word should have length in between 4 and 10
         """
-        monkeypatch.setattr('builtins.input', lambda _: random.choice(string.ascii_letters))
+        self.monkeypatch.setattr('builtins.input', lambda _: random.choice(string.ascii_letters))
         for i in range(-1, 12):
             actual = len(hang.hangman(i))
             assert actual >= 4 or actual <= 10, f"Expect the length of word to be between 4 and 10"
 
-    def test_hangman_word(monkeypatch):
+    def test_hangman_word(self):
         """
         Test if the word is in the set of english word
         """
-        monkeypatch.setattr('builtins.input', lambda _: random.choice(string.ascii_letters))
+        self.monkeypatch.setattr('builtins.input', lambda _: random.choice(string.ascii_letters))
         for i in range(-1, 12):
             actual = hang.hangman(i)
             assert actual in english_words_set, f"Expect word to be in the set of English word"
 
-    def test_hangman_input(monkeypatch):
+    def test_hangman_input(self):
         """
         Test if function works properly with abnormal input
         """
-        monkeypatch.setattr('builtins.input', lambda _: random.choice(string.ascii_letters))
+        self.monkeypatch.setattr('builtins.input', lambda _: random.choice(string.ascii_letters))
         actual = hang.hangman("a")
         assert actual == "code 1", f"Expect the function to return code 1 for abnormal input"
         actual1 = hang.hangman(" ")
